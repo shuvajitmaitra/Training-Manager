fe74bfe5626f4b3312de2c8555531d116afd4ac7
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-10-07 15:45:33 +0600

    user create and and onAuthStateChnage added

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,21 +1,45 @@
 import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+
 import PropTypes from "prop-types";
+import auth from "../Config/firebase.config";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const authContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [services, setServices] = useState([]);
+  const [user, setUser] = useState([]);
 
   useEffect(() => {
     fetch("/data.json")
       .then((response) => response.json())
       .then((data) => setServices(data));
   }, []);
+
+  const createUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+
+console.log(33, user);
+
   const authInfo = {
+    user,
+    createUser,
     services,
   };
-  return <authContext.Provider value={authInfo}>{children}</authContext.Provider>;
+  return (
+    <authContext.Provider value={authInfo}>{children}</authContext.Provider>
+  );
 };
 AuthProvider.propTypes = {
   children: PropTypes.node,
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,14 +1,34 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { authContext } from "../../AuthProvider/AuthProvider";
 
 const Register = () => {
+  const { createUser } = useContext(authContext);
+
+  const handleRegisterBtn = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="flex-1 bg-blue-400 h-full flex justify-center items-center">
         <h2 className="text-3xl font-bold">Welcome back</h2>
       </div>
       <div className="flex-1">
-        <form className="card-body w-3/4 mx-auto">
-            <h3 className="text-xl font-semibold">Log In</h3>
+        <form
+          onSubmit={handleRegisterBtn}
+          className="card-body w-3/4 mx-auto"
+        >
+          <h3 className="text-xl font-semibold">Registration</h3>
           <div className="form-control">
             <label className="label">
               <span className="label-text text-lg ">Full Name</span>
@@ -26,6 +46,7 @@ const Register = () => {
             </label>
             <input
               type="email"
+              name="email"
               placeholder="Enter Email"
               className="input input-bordered rounded"
               required
@@ -37,15 +58,26 @@ const Register = () => {
             </label>
             <input
               type="password"
+              name="password"
               placeholder="Enter Password"
               className="input input-bordered rounded"
               required
             />
           </div>
           <div className="form-control mt-6">
-            <button className="btn btn-primary rounded">Register</button>
+            <button
+              type="submit"
+              className="btn btn-primary rounded"
+            >
+              Register
+            </button>
           </div>
-          <h2 className="mt-2">Already have an account? <Link to="/login"><button className="btn-link">Login</button></Link></h2>
+          <h2 className="mt-2">
+            Already have an account?{" "}
+            <Link to="/login">
+              <button className="btn-link">Login</button>
+            </Link>
+          </h2>
         </form>
       </div>
     </div>
